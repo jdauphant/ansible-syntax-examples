73e8c80bad11293846e64ab4809c3259babdca35
Julien DAUPHANT <judauphant@gmail.com>
2014-02-28 18:59:55 +0100

    Update launch tests scripts

--- examples.sh
+++ examples.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-ansible-playbook -i ansible_hosts -i dev/ansible_hosts -i prod/ansible_hosts main.yml -vv
+ansible-playbook -i ansible_hosts -vv main.yml
+ansible-playbook -i dev -vv main.yml
+ansible-playbook -i prod -vv main.yml
